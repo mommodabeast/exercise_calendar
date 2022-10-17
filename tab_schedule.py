4e4f714ffa4260949de18162778629d2c7e3b039
Ria <kristoffer.berg8@gmail.com>
2022-10-17 15:30:07 +0200

    Added comments to code and added some planning.

--- tab_schedule.py
+++ tab_schedule.py
@@ -12,10 +12,14 @@ from PySide6.QtWidgets import (
 
 # TODO: 
 # limit the number of characters that can be written, and which characters.
-# Fix bug that makes elements in self.items in the class Schedule_scroll_area disappear.
-# Add saving to end button in schedule editor.
+# Add a method to the "schedule_viewer" class to show a schedule.
+# Add method to deal with the case when all schedules in schedule editor is deleted.
 
 class Schedule_editor_item(QWidget):
+    # This class creates an "exercise" that can be placed in a scroll view area. The "exercise"
+    # contains one text field for a name and three for various other things (examples: sets, reps, weight).
+    # The parameter "fields" should be a list of strings.
+    # Note: any text can be written in the fields. 
     def __init__(self, fields):
         super().__init__()
         
@@ -43,16 +47,18 @@ class Schedule_editor_item(QWidget):
             self.entry_list.append(entry_field)
 
     def collect(self):
+        # Method for collecting text input written in the text fields. 
         list_text = []
         list_text.append(self.entry_name.text())
         list_text.append(tuple([input_text.text() for input_text in self.entry_list]))
-         
         return list_text
 
 
 
-
 class Schedule_viewer_item(QWidget):
+    # This class creates an "exercise" that can be placed in a scroll view area.
+    # The "exercise" contains a label that can show any text. The parameter 
+    # "fields" is a list that should contain strings.   
     def __init__(self, name, fields):
         super().__init__()
         
@@ -77,12 +83,16 @@ class Schedule_viewer_item(QWidget):
         layout_main.setDirection(QHBoxLayout.RightToLeft)
         
 
+
 class Schedule_scroll_area(QWidget):
+    # This class creates a scrollable window. 
+    # schedule_items is a list containing objects of 
+    # class "Schedule_viewer_item" or "Schedule_editor_item". 
     def __init__(self, schedule_items):
         super().__init__()
         
-        self.scroll = QScrollArea()             # Scroll Area which contains the widgets, set as the centralWidget
-        self.vbox = QVBoxLayout()               # The Vertical Box that contains the Horizontal Boxes of  labels and buttons
+        self.scroll = QScrollArea()             
+        self.vbox = QVBoxLayout()               
         self.vbox.setSpacing(0)
         self.vbox.setContentsMargins(0, 0, 0, 0)
 
@@ -109,18 +119,19 @@ class Schedule_scroll_area(QWidget):
         item.deleteLater()
     
     def collect(self):
+        # Collect text from all fields in all widgets.
         collection = []
-        print(len(self.items))
         for item in self.items:
             collection.append(item.collect())
 
-        print(collection)
         return collection
 
 
 
-
 class schedule_viewer(QWidget):
+    # This class creates a widget that shows an exercise schedule.
+    # The parameter "schedule" should be a list of tuples, which 
+    # contain a string and a list of three strings.
     def __init__(self, widget_parent, schedule):
         super().__init__()
 
@@ -159,19 +170,20 @@ class schedule_viewer(QWidget):
 
         layout_sub_items.addWidget(self.widget_viewer_items.scroll)
 
-    
     def functionality_edit(self):
         self.widget_parent.layout_main.setCurrentIndex(1)
 
     def viewer_schedule(self):
+        # Creates a scroll area with "exercises" (objects of class "Schedule_viewer_item"). 
         schedule_items = [Schedule_viewer_item(item[0], item[1]) for item in self.schedule]
         self.widget_viewer_items = Schedule_scroll_area(schedule_items)
-
-        
 
 
 
 class Schedule_editor(QWidget):
+    # This class creates a widget that lets a user edit an exercise schedule.
+    # The parameter "schedules" should be a list of tuples, which 
+    # contain a string and a list of three strings.
     def __init__(self, widget_parent, schedules, schedule_index_current):
         super().__init__()
 
@@ -228,22 +240,14 @@ class Schedule_editor(QWidget):
         layout_sub_menu_right.addWidget(menu_button_add_exercise)
         layout_sub_menu_right.addWidget(menu_button_save_schedule)
         
-
-        
-
-        # Items
-        
-               
-
     def set_combo_items(self):
+        # Adds strings to the combo box. 
         for i in range(1, len(self.schedules)+1):
             self.menu_combo.addItem(f"schema {i}")
 
     def update_editor_schedule(self, index):
-
-        # Update index
+        # Updates the scroll area to show the currently selected schedule
         self.schedule_index_current = index
-        print(self.schedules[index])
 
         if self.editor_exists:
             self.layout_sub_items.removeWidget(self.widget_viewer_items.scroll)
@@ -253,67 +257,75 @@ class Schedule_editor(QWidget):
             self.editor_exists = True
             
     def editor_schedule(self, schedule_index):
-        if (len(self.schedules) == 0):
-            return 
-
+        # Creates schedule items from a list of tuples containing
+        # a string and a list of strings. Also creates and sets
+        # a new scroll area which contains the schedule items. 
         schedule_current = self.schedules[schedule_index]
 
         schedule_items = [Schedule_editor_item(item) for item in schedule_current]
         self.widget_viewer_items = Schedule_scroll_area(schedule_items)
         
-    
     def functionality_end(self):
+        # Hides the schedule editor and shows the schedule viewer. 
         self.widget_parent.layout_main.setCurrentIndex(0)
         
     def functionality_add_exercise(self):
+        # Adds a new widget for an exercise and a new tuple in a list.
+        # The list is the list of the list "schedules" with the current index. 
         schedule_items = self.schedules[self.schedule_index_current] 
 
-        # Add exercise widget object and data to schedules.
         item_new = ("name", ["a", "b", "c"])
+        if len(schedule_items) == 0:
+            schedule_items = [("name", ["a", "b", "c"])]
+
         widget_new = Schedule_editor_item(schedule_items[-1])
+            
 
         self.widget_viewer_items.items.append(widget_new)
         self.schedules[self.schedule_index_current].append(item_new)
         self.widget_viewer_items.add(widget_new)
 
     def functionality_remove_exercise(self):
+        # Deletes exercise widget object and removes data from schedules.
         schedule_items = self.schedules[self.schedule_index_current] 
         list_sub = self.schedules[self.schedule_index_current]
 
         # Exit function if no exercise widget objects exist.
-        if len(list_sub) == 1:
+        if len(list_sub) == 0:
             return
 
-        # Delete exercise widget object and remove data from schedules.
         self.widget_viewer_items.remove(self.widget_viewer_items.items[-1])
         del list_sub[-1]
         del self.widget_viewer_items.items[-1]
         
-
     def functionality_add_schedule(self):
+        # Adds a new schedule to both the list containing all schedules
+        # and the combo box.
         schedule_new_name = f"schema {len(self.schedules)+1}"
         self.schedules.append([("name1", ["a", "b", "c"])])
         self.menu_combo.addItem(schedule_new_name)
     
     def functionality_remove_schedule(self):
+        # Removes a schedule from both list and combo box. 
         del self.schedules[self.schedule_index_current]
         self.menu_combo.removeItem(self.schedule_index_current)
 
     def functionality_save(self):
+        # Saves text written to fields.
         self.schedules[self.schedule_index_current] = self.widget_viewer_items.collect()
 
 
 
-
 class tab_schedule(QWidget):
+    # Creates a "tab" (widget) in which the user can view and edit schedules. 
     def __init__(self):
         super().__init__()
 
         # Test variables and attributes
         schedules = [
-            [("name1", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])],
-            [("name2", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])],
-            [("name3", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])]
+            [("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])],
+            [("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])],
+            [("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"]), ("name", ["a", "b", "c"])]
         ]
 
         # Layout of tab
